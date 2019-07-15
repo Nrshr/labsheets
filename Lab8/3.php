0bd85637808705ff9b0a49e341ad182aee53185c
NareshRoka <50292589+Nrshr@users.noreply.github.com>
2019-07-15 09:36:37 +0545

    Add files via upload

---
+++ Lab8/3.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BeginPHP</title>
+</head>
+<body>
+	<?php 
+	$a="Hello to u all!";
+	$x=1;
+	$y=1.5;
+	echo "$a<br>";
+	define("Oi","Hello again");
+	echo constant("Oi");echo " is a constant here.<br>";
+	define("c","5");
+	echo c;echo " is a constant here.<br>";
+echo date("l h:i:s A ");
+$t = date("h");
+
+if ($t < "10") {
+    echo "<br>Have a good morning!";
+} elseif ($t < "20") {
+    echo "<br>Have a good day!";
+} else {
+    echo "<br>Have a good night!";
+
+}
+}
+	?>
+</body>
+</html>
---
+++ Lab8/5.php
@@ -0,0 +1,17 @@
+  <!DOCTYPE html>
+  <html>
+  <head>
+  	<title></title>
+  </head>
+  <body>
+<?php 
+$fruits = array('Mango',"Orange","Banana","Pear","Grapes","Apple","Peach","Guava","Papaya","Strawberry" );
+foreach ($fruits as $value) {
+	echo "$value<br>";}
+ ?>
+
+  
+  </body>
+  </html>
+
+  
